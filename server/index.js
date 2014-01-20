de4c78164ef4e54ec55f331b421b9f23a18a4219
ryknow <ryanknowles@gmail.com>
2014-01-20 14:33:30 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
-Meteor.startup(function() {
-  // code to run on server at startup
-  // splash page
+Meteor.methods({
+  start_new_game: function() {
+    var turns_left = 30;
+    var game_id = Games.insert({board: new_board, turns_left: turns_left});
+    Players.update({game_id: null, name: {$ne: ''}}, 
+                   {$set: {game_id: game_id}}, 
+                   {multi: true});
+    var players = Players.find({game_id: game_id},
+                               {fields: {_id: true, name: true}}).fetch();
+    Games.update({_id: game_id}, {$set: {players: players}});
+    
+    // TODO: Loop through players
+    // After each gets a turn decrement turns_left by 1 and update the game
+    if (turns_left === 0) {
+      console.log("Game Over");
+    }
+  }
 });
