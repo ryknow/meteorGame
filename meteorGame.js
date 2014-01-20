e34378e7a831e5b7991b8ef72b3bf4c26e761021
ryknow <ryanknowles@gmail.com>
2014-01-20 12:58:58 -0500

    Create meteorGame.js
    Working on logic to create new board

---
+++ meteorGame.js
@@ -0,0 +1,61 @@
+Games   = new Meteor.Collection('games');
+Players = new Meteor.Collection('players');
+
+row_size = 5;
+
+var cell = function(index, additional_class) {
+  return {
+    cell_num: index,
+    players: [],
+    enemies: [],
+    corner: additional_class,
+    has_player: function() {
+      this.players.length > 0;
+    },
+    has_enemies: function() {
+      this.enemies.length > 0;
+    },
+    is_building: function() {
+      return this.corner !== "" ? true : false;
+    },
+    adjacent_cells: [
+      this.cell_num - row_size,
+      this.cell_num + row_size,
+      this.cell_num - 1,
+      this.cell_num + 1,
+      this.cell_num - (row_size + 1),
+      this.cell_num - (row_size - 1),
+      this.cell_num + (row_size + 1),
+      this.cell_num + (row_size - 1)
+    ]
+  }
+}
+
+new_board: function() {
+  var board = [];
+  var i;
+  
+  for (i = 0; i < 24; i++) {
+    var additional_class;
+    switch(i) {
+      case 0: 
+        additional_class = "top-left";
+        break;
+      case 4: 
+        additional_class = "top-right";
+        break;
+      case 19:
+        additional_class = "bottom-left";
+        break;
+      case 24:
+        additional_class = "bottom-right";
+        break;
+      default:
+        additional_class = "";
+    }
+    
+    board[i] = cell(i, additional_class);
+  }
+  
+  return board;
+}
